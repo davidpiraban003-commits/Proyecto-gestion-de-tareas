3263cc44e762d7411068b83d80a3fa6c26c30cca
David <davidpiraban003@gmail.com>
2026-06-28 15:35:10 -0500

    feat: Inicio de proyecto y creacion de modulos

---
+++ src/main/java/David/gestion/de/tareas/GestionDeTareasApplication.java
@@ -0,0 +1,13 @@
+package David.gestion.de.tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/David/gestion/de/tareas/GestionDeTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.David.gestion.de.tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
